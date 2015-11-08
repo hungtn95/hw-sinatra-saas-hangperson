44f1fee78b3f6f2c1479d2cbec4af4cbad0ca9cc
hungtn95 <hungtn95@gmail.com>
2015-11-08 10:05:16 +0000

    Guess repeated letter feature

--- app.rb
+++ app.rb
@@ -40,6 +40,9 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if @game.guess(letter) == false
+      flash[:message] = "You have already used that letter"
+    end
     redirect '/show'
   end
   

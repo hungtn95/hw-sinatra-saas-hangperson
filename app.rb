096c6cde9080fd30ae7ddabc6198b4aca7679883
hungtn95 <hungtn95@gmail.com>
2015-11-08 12:22:34 +0000

    Handle cheating

--- app.rb
+++ app.rb
@@ -41,7 +41,7 @@ class HangpersonApp < Sinatra::Base
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
     begin
-      if @game.guess(letter) == false
+      if !@game.guess(letter)
         flash[:message] = "You have already used that letter."
       end
     rescue
@@ -57,10 +57,12 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    if @game.check_win_or_lose == :lose
-      redirect '/lose'
-    elsif @game.check_win_or_lose == :win
-      redirect '/win'
+    if params[:guess] != nil
+      if @game.check_win_or_lose == :lose
+        redirect '/lose'
+      elsif @game.check_win_or_lose == :win
+        redirect '/win'
+      end
     end
     erb :show # You may change/remove this line
   end

f4c33c325719ca026d1a498ff3d9dc1fa0112cdb
hungtn95 <hungtn95@gmail.com>
2015-11-08 11:42:38 +0000

    Exception handling

--- app.rb
+++ app.rb
@@ -40,9 +40,13 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if @game.guess(letter) == false
-      flash[:message] = "You have already used that letter."
-    elsif @game.invalid == true
+    begin
+      if @game.guess(letter) == false
+        flash[:message] = "You have already used that letter."
+      elsif @game.invalid == true
+        flash[:message] = "Invalid guess."
+      end
+    rescue
       flash[:message] = "Invalid guess."
     end
     redirect '/show'
